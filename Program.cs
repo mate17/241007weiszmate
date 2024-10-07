a7c0b638b9d44348c8d88fd4534836eecfe261f5
weisz.mate <weisz.mate@28-DIAK14>
2024-10-07 14:46:18 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using _241007weiszmate;
+using System.Text;
+
+List<Verseny> versenyzok = [];
+
+using StreamReader sr = new(@"..\..\..\forras.txt", Encoding.UTF8);
+while (!sr.EndOfStream) versenyzok.Add(new(sr.ReadLine()));
+
+//1.feladat
+Console.WriteLine($"A versenyen résztvevők száma: {versenyzok.Count()}");
+
+//2.feladat
+var fel2 = versenyzok.Count(v => v.kategoria == "25-29");
+Console.WriteLine($"versenyzők száma 25-29-es kat-ban: {fel2}");
+
+//3. feladat
+var fel3 = versenyzok.Average(v => 2024 - v.ev);
+Console.WriteLine($"Átlag életkor: {fel3:0} év");
+
+//7.feladat
+var fel7 = versenyzok.GroupBy(v =>  v.kategoria);
+foreach (var fel in fel7)
+{
+    Console.WriteLine($"Kategória: {fel.Key}\n\t{fel.Count()} db játékos");
+}
+
+Console.ReadKey();
---
+++ Verseny.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _241007weiszmate
+{
+    internal class Verseny
+    {
+        public string nev { get; set; }
+        public int ev { get; set; }
+        public int rajtszam { get; set; }
+        public string nem { get; set; }
+        public string kategoria { get; set; }
+        public TimeOnly uszasideje { get; set; }
+        public TimeOnly elsodepobantoltottido { get; set; }
+        public TimeOnly kerekparozasideje { get; set; }
+        public TimeOnly masodikdepobantoltottido { get; set; }
+        public TimeOnly futasideje { get; set; }
+
+        public override string ToString()
+        {
+            return $"Név: {nev} szülév: {ev} száma: {rajtszam} neme:{nem} kati: {kategoria} idok: {uszasideje}, {elsodepobantoltottido}, {kerekparozasideje}, {masodikdepobantoltottido} futás idő: {futasideje}";
+        }
+
+        public Verseny(string sor)
+        {
+            var v = sor.Split(';');
+            nev = v[0];
+            ev = int.Parse(v[1]);
+            rajtszam = int.Parse(v[2]);
+            nem = v[3];
+            kategoria = v[4];
+            uszasideje = TimeOnly.Parse(v[5]);
+            elsodepobantoltottido = TimeOnly.Parse(v[6]);
+            kerekparozasideje = TimeOnly.Parse(v[7]);
+            masodikdepobantoltottido= TimeOnly.Parse(v[8]);
+            futasideje = TimeOnly.Parse(v[9]);
+        }
+    }
+}
